642a22b5faa44fd62eadd6f352474c921a9c155a
riiy zhou <riiyzhou@gmail.com>
2023-03-27 11:48:24 +0800

    update git ignore

--- test/source/acquire_money.cpp
+++ test/source/acquire_money.cpp
@@ -7,15 +7,15 @@
 TEST_CASE("Greeter") {
   using namespace greeter;
 
-  Greeter greeter("Tests");
+  Greeter greeter("测试");
 
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+  CHECK(greeter.greet(LanguageCode::EN) == "Hello, 测试!");
+  CHECK(greeter.greet(LanguageCode::DE) == "Hallo 测试!");
+  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola 测试!");
+  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour 测试!");
 }
 
-TEST_CASE("Greeter version") {
+TEST_CASE("AcquireMoney version") {
   static_assert(std::string_view(ACQUIREMONEY_VERSION) == std::string_view("1.0"));
   CHECK(std::string(ACQUIREMONEY_VERSION) == std::string("1.0"));
 }
