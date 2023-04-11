3289632cbd89c9ab5b1b1e4813b07357359177b3
riiy zhou <riiyzhou@gmail.com>
2023-04-11 14:50:16 +0800

    replace csv reader

---
+++ csv2ch/source/daily_data.cpp
@@ -0,0 +1,82 @@
+#include <clickhouse/columns/decimal.h>
+#include <clickhouse/columns/numeric.h>
+#include <clickhouse/columns/string.h>
+
+#include <filesystem>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+namespace fs = std::filesystem;
+
+#include <clickhouse/client.h>
+
+#include "csv.h"
+
+std::string get_stem(const fs::path& p) { return p.stem().string(); }
+
+auto daily_data() -> int {
+  clickhouse::Client client(clickhouse::ClientOptions().SetHost("localhost").SetPort(19000));
+  // CREATE OR REPLACE TABLE daily_data (
+  // symbol String,
+  // date String,
+  // open Float64,
+  // high Float64,
+  // low Float64,
+  // close Float64,
+  // volume Float64,
+  // outstanding_share Float64,
+  // turnover Float64,
+  // updated_at DateTime('Asia/Shanghai') DEFAULT now(),
+  // updated_at_date Date DEFAULT toDate(updated_at)
+  // ) ENGINE = MergeTree() ORDER BY (symbol, date);
+  std::string path = "/data/stock/daily_data_qfq";
+  for (const auto& entry : fs::directory_iterator(path)) {
+    break;
+    clickhouse::Block block;
+
+    auto symbol = std::make_shared<clickhouse::ColumnString>();
+    auto _date = std::make_shared<clickhouse::ColumnString>();
+    auto _open = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _high = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _low = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _close = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _volume = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _outstanding_share = std::make_shared<clickhouse::ColumnFloat64>();
+    auto _turnover = std::make_shared<clickhouse::ColumnFloat64>();
+
+    io::CSVReader<8> in(entry.path().c_str());
+    in.read_header(io::ignore_extra_column, "date", "open", "high", "low", "close", "volume",
+                   "outstanding_share", "turnover");
+    std::string date;
+    long double open;
+    long double high;
+    long double low;
+    long double close;
+    long double volume;
+    long double outstanding_share;
+    long double turnover;
+    while (in.read_row(date, open, high, low, close, volume, outstanding_share, turnover)) {
+      symbol->Append(get_stem(entry));
+      _date->Append(date);
+      _open->Append(open);
+      _high->Append(high);
+      _low->Append(low);
+      _close->Append(close);
+      _volume->Append(volume);
+      _outstanding_share->Append(outstanding_share);
+      _turnover->Append(turnover);
+    }
+    block.AppendColumn("symbol", symbol);
+    block.AppendColumn("date", _date);
+    block.AppendColumn("open", _open);
+    block.AppendColumn("high", _high);
+    block.AppendColumn("low", _low);
+    block.AppendColumn("close", _close);
+    block.AppendColumn("volume", _volume);
+    block.AppendColumn("outstanding_share", _outstanding_share);
+    block.AppendColumn("turnover", _turnover);
+    client.Insert("default.daily_data", block);
+  }
+  return 0;
+}
--- csv2ch/source/main.cpp
+++ csv2ch/source/main.cpp
@@ -1,79 +1,53 @@
-#include <assert.h>
-#include <clickhouse/columns/date.h>
-#include <clickhouse/columns/decimal.h>
-#include <clickhouse/columns/numeric.h>
-#include <clickhouse/columns/string.h>
-#include <locale.h>
-#include <time.h>
+#include <acquiremoney/greeter.h>
+#include <acquiremoney/version.h>
 
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <filesystem>
+#include <cxxopts.hpp>
 #include <iostream>
-#include <ostream>
 #include <string>
+#include <unordered_map>
 
-namespace fs = std::filesystem;
+auto main(int argc, char** argv) -> int {
+  const std::unordered_map<std::string, greeter::LanguageCode> languages{
+      {"en", greeter::LanguageCode::EN},
+      {"de", greeter::LanguageCode::DE},
+      {"es", greeter::LanguageCode::ES},
+      {"fr", greeter::LanguageCode::FR},
+  };
 
-#include <clickhouse/client.h>
+  cxxopts::Options options(*argv, "A program to welcome the world!");
 
-#include "csv.hpp"
+  std::string language;
+  std::string name;
 
-std::string get_stem(const fs::path& p) { return p.stem().string(); }
+  // clang-format off
+  options.add_options()
+    ("h,help", "Show help")
+    ("v,version", "Print the current version number")
+    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
+    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
+  ;
+  // clang-format on
 
-auto main() -> int {
-  clickhouse::Client client(clickhouse::ClientOptions().SetHost("localhost").SetPort(19000));
-  // CREATE OR REPLACE TABLE daily_data (
-  // symbol String,
-  // date String,
-  // open Float64,
-  // high Float64,
-  // low Float64,
-  // close Float64,
-  // volume Float64,
-  // outstanding_share Float64,
-  // turnover Float64,
-  // updated_at DateTime('Asia/Shanghai') DEFAULT now(),
-  // updated_at_date Date DEFAULT toDate(updated_at)
-  // ) ENGINE = MergeTree() ORDER BY (symbol, date);
-  std::string path = "/data/stock/daily_data_qfq";
-  for (const auto& entry : fs::directory_iterator(path)) {
-    break;
-    clickhouse::Block block;
+  auto result = options.parse(argc, argv);
 
-    auto symbol = std::make_shared<clickhouse::ColumnString>();
-    auto date = std::make_shared<clickhouse::ColumnString>();
-    auto open = std::make_shared<clickhouse::ColumnFloat64>();
-    auto high = std::make_shared<clickhouse::ColumnFloat64>();
-    auto low = std::make_shared<clickhouse::ColumnFloat64>();
-    auto close = std::make_shared<clickhouse::ColumnFloat64>();
-    auto volume = std::make_shared<clickhouse::ColumnFloat64>();
-    auto outstanding_share = std::make_shared<clickhouse::ColumnFloat64>();
-    auto turnover = std::make_shared<clickhouse::ColumnFloat64>();
-
-    csv::CSVReader reader(entry.path().c_str());
-    for (auto& row : reader) {
-      symbol->Append(get_stem(entry));
-      date->Append(row["date"].get());
-      open->Append(row["open"].get<long double>());
-      high->Append(row["high"].get<long double>());
-      low->Append(row["low"].get<long double>());
-      close->Append(row["close"].get<long double>());
-      volume->Append(row["volume"].get<long double>());
-      outstanding_share->Append(row["outstanding_share"].get<long double>());
-      turnover->Append(row["turnover"].get<long double>());
-    }
-    block.AppendColumn("symbol", symbol);
-    block.AppendColumn("date", date);
-    block.AppendColumn("open", open);
-    block.AppendColumn("high", high);
-    block.AppendColumn("low", low);
-    block.AppendColumn("close", close);
-    block.AppendColumn("volume", volume);
-    block.AppendColumn("outstanding_share", outstanding_share);
-    block.AppendColumn("turnover", turnover);
-    client.Insert("default.daily_data", block);
+  if (result["help"].as<bool>()) {
+    std::cout << options.help() << std::endl;
+    return 0;
   }
+
+  if (result["version"].as<bool>()) {
+    std::cout << "Greeter, version " << ACQUIREMONEY_VERSION << std::endl;
+    return 0;
+  }
+
+  auto langIt = languages.find(language);
+  if (langIt == languages.end()) {
+    std::cerr << "unknown language code: " << language << std::endl;
+    return 1;
+  }
+
+  greeter::Greeter greeter(name);
+  std::cout << greeter.greet(langIt->second) << std::endl;
+
   return 0;
 }
