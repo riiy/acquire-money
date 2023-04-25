429efc3ef6a7b71917bda4ee0a8044bb9c987c4c
riiy zhou <riiyzhou@gmail.com>
2023-04-25 16:11:16 +0800

    delete greeter

--- test/source/acquire_money.cpp
+++ test/source/acquire_money.cpp
@@ -1,20 +1,8 @@
-#include <acquiremoney/greeter.h>
 #include <acquiremoney/stock_feature.h>
 #include <acquiremoney/version.h>
 #include <doctest/doctest.h>
 
 #include <string>
-
-TEST_CASE("Greeter") {
-  using namespace greeter;
-  Greeter greeter("测试");
-
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, 测试!");
-  CHECK(greeter.greet(LanguageCode::DE) == "Hallo 测试!");
-  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola 测试!");
-  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour 测试!");
-}
-
 TEST_CASE("AcquireMoney version") {
   static_assert(std::string_view(ACQUIREMONEY_VERSION) == std::string_view("1.0"));
   CHECK(std::string(ACQUIREMONEY_VERSION) == std::string("1.0"));
