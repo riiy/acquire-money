bf532c9065d32056955ec7abdbfe532ef01160d1
riiy zhou <riiyzhou@gmail.com>
2023-04-25 16:01:55 +0800

    delete no use code

--- csv2ch/include/csv2ch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <filesystem>
-#include <string>
-
-namespace fs = std::filesystem;
-inline std::string get_stem(const fs::path& p) { return p.stem().string(); }
-int daily_data(const std::string& path);
-int tick_data(const std::string& path);
